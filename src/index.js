d5ce78709a5b16bbd340b728788950aa4c33b9df
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-06-28 20:28:48 -0700

    solution for fn that takes 3 nums invoked 3 times from one fn.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log("i work");
+/* What is a "mul" function : https://www.geeksforgeeks.org/explain-the-mul-function-in-javascript/ */
+
+/* example
+    make this function work :
+    console.log(mul(2)(3)(4)); // output : 24
+    console.log(mul(4)(3)(4)); // output : 48
+*/
+
+const mul = (x) => {
+  return (y) => {
+    return (z) => {
+      console.log(x, y, z);
+      return x * y * z;
+    };
+  };
+};
+
+const mulResult = mul(3)(3)(2);
+console.log("mulResult", mulResult);
