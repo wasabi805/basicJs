d44cb530229851ba3a91001b6526b8c201b93702
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-06-28 20:01:59 -0700

    added solution.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 console.log('i work')
+/*soure : https://www.fullstack.cafe/blog/javascript-code-interview-questions */
+
+// Implement enqueue and dequeue using only two stacks
+//  Enqueue means to add an element, dequeue to remove an element.
+
+const inputStack = []
+const dequeueStack = []
+
+const enqueue = (arr, item)=>{
+    arr.push(item)
+}
+
+enqueue(inputStack, 1)
+enqueue(inputStack, 2)
+enqueue(inputStack, 3)
+enqueue(inputStack, 4)
+
+console.log(inputStack)
+
+const dequeue = (input, dequeue)=>{
+      // Reverse the stack such that the first element of the output stack is the
+        // last element of the input stack. After that, pop the top of the output to
+  // get the first element that was ever pushed into the input stack
+
+    if(dequeue <=0){
+        
+        while(input.length > 0 ){
+            
+            const itemToDisplay = input.pop()
+            console.log(itemToDisplay)
+            dequeue.push(itemToDisplay  )
+            console.log(dequeue)
+        }
+    }
+    return dequeue.pop()
+}
+
+const dequeValue= dequeue(inputStack, dequeueStack)
+
+console.log('this is the dequeValue', dequeValue)
